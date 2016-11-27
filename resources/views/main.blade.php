c75f6f45ec6ab95d75354926513b713ae262228e
Andy Melentev <melentev.av@gmail.com>
2016-11-27 05:14:02 +0500

    fix url ok

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -280,6 +280,9 @@
 
                 <INPUT TYPE="HIDDEN" NAME="OrderNumber" VALUE="">
                 <INPUT TYPE="HIDDEN" NAME="OrderAmount" VALUE="">
+                <INPUT TYPE="HIDDEN" NAME="Firstname" VALUE="">
+                <INPUT TYPE="HIDDEN" NAME="Middlename" VALUE="">
+                <INPUT TYPE="HIDDEN" NAME="Lastname" VALUE="">
                     <div class="calc-form-item calc-form-item__required">
                         <input name="FirstName" class="calc-field calc-js-user_field" placeholder="Имя">
                         <div class="calc-error-message calc-hidden">Пожалуйста, введите имя</div>
