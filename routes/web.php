0280d04c5e9bff54e7044fd5ad926d4a517bacc0
Andy Melentev <melentev.av@gmail.com>
2016-11-27 09:06:08 +0500

    fix

--- routes/web.php
+++ routes/web.php
@@ -69,7 +69,7 @@ Route::group(['prefix' => 'api'], function (){
 
 });
     Route::any('ok', 'PaymentController@setOk');
-    Route::get('ok', 'PaymentController@setNo');
+    Route::any('ok', 'PaymentController@setNo');
 
 Auth::routes();
 
