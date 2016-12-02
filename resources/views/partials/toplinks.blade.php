e18fa078269cd21cda26851930c362c81a4bcd26
Andy Melentev <melentev.av@gmail.com>
2016-12-02 06:33:42 +0500

    fix toplinks

--- resources/views/partials/toplinks.blade.php
+++ resources/views/partials/toplinks.blade.php
@@ -4,7 +4,6 @@
         <div class="calc-top-links-inside">
             @foreach($tech_stack as $tech)
                 <p><a href="{{url('/').'/'.$tech->file}}"><i class="fa fa-file-pdf-o" aria-hidden="true"></i> {{$tech->name}}</a></p>
-                <p><a href="#"><i class="fa fa-file-pdf-o" aria-hidden="true"></i> Инструкция по монтажу и эксплуатации столешниц.pdf</a></p>
             @endforeach
         </div>
     </div>
