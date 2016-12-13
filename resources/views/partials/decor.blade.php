b1a5575b132ae00da932f7e8bfb659579157bc26
Andy Melentev <melentev.av@gmail.com>
2016-12-13 15:15:53 +0500

    Fix img

--- resources/views/partials/decor.blade.php
+++ resources/views/partials/decor.blade.php
@@ -10,9 +10,9 @@
                  data-series-name="{{$decor->decorCategory->name}}"
             >
                 <img src="{{url('/').'/css/img/galochka.png'}}" alt="" class="calc-modal-check">
-                <div class="cub"></div>
+                <div class="cub{{$decor->id}}"></div>
                 <style>
-                    .cub { width:100px; height:100px; background: url({{url('/').'/'.$decor->image}}) center; background-size: cover; }
+                    .cub{{$decor->id}} { width:100px; height:100px; background: url({{url('/').'/'.$decor->image}}) center; background-size: cover; }
                 </style>
                 {{--<img src="@if(!empty($decor->image)){{url('/').'/'.$decor->image}}@endif" alt="">--}}
                 <span class="calc-modal-name">{{$decor->name}} {{$decor->code}} ({{$decor->decorCategory->name}})</span>
