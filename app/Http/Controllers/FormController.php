f0b138b30c048e1fcf3976fe165f110bc744a743
Andy Melentev <melentev.av@gmail.com>
2016-12-19 16:38:45 +0500

    some fix

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -27,14 +27,14 @@ class FormController extends Controller
             ->first();
 
         return response()->json([
-            'option_edge_1' => $response->option_edge_1,
-            'option_edge_2' => $response->option_edge_2,
-            'option_edge_3' => $response->option_edge_3,
-            'option_edge_4' => $response->option_edge_4,
-            'option_side_1' => $response->option_side_1,
-            'option_side_2' => $response->option_side_2,
-            'option_side_3' => $response->option_side_3,
-            'option_side_4' => $response->option_side_4,
+            'option_edge_1' => $response->option_edge_1->id,
+            'option_edge_2' => $response->option_edge_2->id,
+            'option_edge_3' => $response->option_edge_3->id,
+            'option_edge_4' => $response->option_edge_4->id,
+            'option_side_1' => $response->option_side_1->id,
+            'option_side_2' => $response->option_side_2->id,
+            'option_side_3' => $response->option_side_3->id,
+            'option_side_4' => $response->option_side_4->id,
         ]);
     }
 }
