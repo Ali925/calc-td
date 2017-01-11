a77083589831af910a3967300bd2170b6199f4a3
Andy Melentev <melentev.av@gmail.com>
2017-01-11 16:56:38 +0500

    docs page users

--- resources/views/docs/user.blade.php
+++ resources/views/docs/user.blade.php
@@ -1,7 +1,95 @@
 <div class="container">
+    <div class="col-md-10">
 
-    <p>
+        <h4 class="text-success">
+            Этот раздел доступен: Администраторам
+        </h4>
 
-    </p>
+        <div class="col-md-12">
+            <p>В данном разделе показана таблица пользователей имеющих доступ к административной
+                части калькулятора. В таблице указаны все основные данные о пользователях:
+            </p>
 
+            <ul>
+                <li>Имя пользователя</li>
+                <li>Электронная почта</li>
+                <li>Роль</li>
+                <li>Дейставия</li>
+            </ul>
+        </div>
+
+        <div class="col-md-12" style="border-bottom: 2px solid">
+            <span class="text-center"><img src="/images/doc/2.1.png" class="img-responsive"></span>
+            <h5 class="text-center">(Рисунок 2.1)</h5>
+        </div>
+
+        <div class="col-md-12" style="margin-top: 20px">
+            <p>
+                <h3>Добавление пользователя</h3>
+                Чтобы добавить пользователя в систему необходимо нажать кнопку “New Entry” в
+                верхнем левом углу таблицы.
+            </p>
+        </div>
+
+        <div class="col-md-12">
+            <span class="text-center"><img src="/images/doc/2.1.1.png" class="img-responsive"></span>
+            <h5 class="text-center">(Рисунок 2.1.1)</h5>
+        </div>
+
+        <div class="col-md-12">
+            <p>
+                Перед вами откроется страница для создания нового пользователя.
+            </p>
+        </div>
+
+        <div class="col-md-12" style="border-bottom: 2px solid">
+            <span class="text-center"><img src="/images/doc/2.1.2.png" class="img-responsive"></span>
+            <h5 class="text-center">(Рисунок 2.1.2)</h5>
+        </div>
+
+        <div class="col-md-12" style="margin-top: 20px">
+            <p>
+            <h3>Редактирование пользователя</h3>
+            Чтобы начать редактирование существующего пользователя нужно в основной таблице пользователей нажать
+            на изображение “Карандаша” в конце строки с нужным пользователем.
+            </p>
+        </div>
+
+        <div class="col-md-12">
+            <span class="text-center"><img src="/images/doc/2.2.1.png" class="img-responsive"></span>
+            <h5 class="text-center">(Рисунок 2.2.1)</h5>
+        </div>
+
+        <div class="col-md-12">
+            <p>
+                Нажатие по этой кнопке приведет вас на страницу редактирования пользователя.
+                Для завершения  редактирования нажать кнопку “Save”, в случае если вы
+                передумали редактировать запись но нажать “Cancel”.
+            </p>
+        </div>
+
+        <div class="col-md-12" style="border-bottom: 2px solid">
+            <span class="text-center"><img src="/images/doc/2.2.2.png" class="img-responsive"></span>
+            <h5 class="text-center">(Рисунок 2.2.2)</h5>
+        </div>
+
+        <div class="col-md-12" style="margin-top: 20px">
+            <p>
+            <h3>Удаление пользователя</h3>
+            Для удаления пользователя нужно на экране редактирование или на экране общей таблицы пользователей
+            нажать красную кнопку с изображением “Мусорной корзины”.
+            </p>
+        </div>
+
+        <div class="col-md-12">
+            <span class="text-center"><img src="/images/doc/2.3.1.png" class="img-responsive"></span>
+            <h5 class="text-center">(Рисунок 2.3.1)</h5>
+        </div>
+
+        <div class="col-md-12" style="border-bottom: 2px solid">
+            <span class="text-center"><img src="/images/doc/2.3.2.png" class="img-responsive"></span>
+            <h5 class="text-center">(Рисунок 2.3.2)</h5>
+        </div>
+
+    </div>
 </div>
