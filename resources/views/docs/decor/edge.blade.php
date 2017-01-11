46002c05c8ad48e18c47232ff3b7675767c8c587
Andy Melentev <melentev.av@gmail.com>
2017-01-11 22:16:16 +0500

    more decors page category

--- resources/views/docs/decor/edge.blade.php
+++ resources/views/docs/decor/edge.blade.php
@@ -2,7 +2,7 @@
 <div class="col-md-12">
     <h4 id="edge-category">Категории</h4>
     <p>
-        В данном разделе заполняются данные для категории декора изделия.
+        В данном разделе заполняются данные для категории декора кромок.
     </p>
 </div>
 
@@ -97,7 +97,100 @@
     <h5 class="text-center">(Рисунок 3.2.9)</h5>
 </div>
 
-<h4 id="edge-decor">Декор</h4>
-<h4 id="edge-decor-create">Добавить декор</h4>
-<h4 id="edge-decor-edit">Редактировать декор</h4>
-<h4 id="edge-decor-delete">Удалить декор</h4>
+
+<div class="col-md-12" style="margin-top: 20px">
+    <h4 id="edge-decor">Декор</h4>
+    <p>
+        В данном разделе заполняются декоры кромок.
+    </p>
+</div>
+
+<div class="col-md-12" style="border-bottom: 2px solid">
+    <span class="text-center"><img src="/images/doc/3.1.10.png" class="img-responsive"></span>
+    <h5 class="text-center">(Рисунок 3.2.10)</h5>
+</div>
+
+<div class="col-md-12" style="margin-top: 20px">
+    <h4 id="edge-decor-create">Добавить декор</h4>
+    <p>
+        Чтобы добавить декор нужно нажать на кнопку “New Entry” в левом верхнем углу таблицы
+    </p>
+</div>
+
+<div class="col-md-12">
+    <span class="text-center"><img src="/images/doc/3.1.11.png" class="img-responsive"></span>
+    <h5 class="text-center">(Рисунок 3.2.11)</h5>
+</div>
+
+<div class="col-md-12">
+    <p>
+        Заполните все поля в появившемся окне.
+    </p>
+</div>
+
+<div class="col-md-12">
+    <span class="text-center"><img src="/images/doc/3.1.12.png" class="img-responsive"></span>
+    <h5 class="text-center">(Рисунок 3.2.12)</h5>
+</div>
+
+<div class="col-md-12">
+    <p>
+        Нажмите кнопку "Save".
+    </p>
+</div>
+
+<div class="col-md-12" style="border-bottom: 2px solid">
+    <span class="text-center"><img src="/images/doc/3.1.13.png" class="img-responsive"></span>
+    <h5 class="text-center">(Рисунок 3.2.13)</h5>
+</div>
+
+<div class="col-md-12" style="margin-top: 20px">
+    <h4 id="edge-decor-edit">Редактировать декор</h4>
+    <p>
+        Чтобы редактировать существующую запись в таблице нужно нажать кнопку в виде “Карандаша”
+    </p>
+</div>
+
+<div class="col-md-12">
+    <span class="text-center"><img src="/images/doc/3.1.14.png" class="img-responsive"></span>
+    <h5 class="text-center">(Рисунок 3.2.14)</h5>
+</div>
+
+<div class="col-md-12">
+    <p>
+        В появившемся окне изменить необходимые данные
+    </p>
+</div>
+
+<div class="col-md-12">
+    <span class="text-center"><img src="/images/doc/3.1.15.png" class="img-responsive"></span>
+    <h5 class="text-center">(Рисунок 3.2.15)</h5>
+</div>
+
+<div class="col-md-12">
+    <p>
+        Нажать кнопку "Save"
+    </p>
+</div>
+
+<div class="col-md-12" style="border-bottom: 2px solid">
+    <span class="text-center"><img src="/images/doc/3.1.16.png" class="img-responsive"></span>
+    <h5 class="text-center">(Рисунок 3.2.16)</h5>
+</div>
+
+<div class="col-md-12" style="margin-top: 20px">
+    <h4 id="edge-decor-delete">Удалить декор</h4>
+    <p>
+        Удалить декор можно во время редактирования или находясь на экране главной таблицы
+    </p>
+</div>
+
+<div class="col-md-12">
+    <span class="text-center"><img src="/images/doc/3.1.17.png" class="img-responsive"></span>
+    <h5 class="text-center">(Рисунок 3.2.17)</h5>
+</div>
+
+<div class="col-md-12">
+    <span class="text-center"><img src="/images/doc/3.1.18.png" class="img-responsive"></span>
+    <h5 class="text-center">(Рисунок 3.2.18)</h5>
+</div>
