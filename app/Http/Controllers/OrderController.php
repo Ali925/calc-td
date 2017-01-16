a2952d324b138d10154444a3a2c793c399289729
Andy Melentev <melentev.av@gmail.com>
2017-01-16 19:13:21 +0500

    test

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -55,59 +55,64 @@ class OrderController extends Controller
 
     public function setReadyProduct(Request $request)
     {
+
+        $post = print_r($_POST, TRUE);
+        file_put_contents(public_path().'post_content.txt', $post, FILE_APPEND);
+        $prs = print_r($request->ready_product, TRUE);
+        file_put_contents(public_path().'post_content.txt', $prs, FILE_APPEND);
+        die();
+
         $coast = [];
         $order = Order::find($request->order_id);
 
-        return $request->all();
+        if (!empty($request->ready_product)){
 
-//        if (!empty($request->ready_product)){
-//
-//            foreach ($request->ready_product as $product){
-//
-//                $readyProduct = new ReadyProduct();
-//                $readyProduct->blank_type_id = $product['blank_type_id'];
-//                $readyProduct->thickness_id = $product['thickness_id'];
-//                $readyProduct->form_id = $product['form_id'];
-//                $readyProduct->decor_category_id = $product['decor_category_id'];
-//                $readyProduct->decor_id = $product['decor_id'];
-//                $readyProduct->wrapper_id = $product['wrapper_id'];
-//                $readyProduct->pattern_accordance_id = $product['pattern_accordance_id'];
-//
-//                $readyProduct->part_side_one = $product['part_side_one'];
-//                $readyProduct->part_side_two = $product['part_side_two'];
-//                $readyProduct->part_side_three = $product['part_side_three'];
-//                $readyProduct->part_side_four = $product['part_side_four'];
-//                $readyProduct->part_edge_one = $product['part_edge_one'];
-//                $readyProduct->part_edge_two = $product['part_edge_two'];
-//                $readyProduct->part_edge_three = $product['part_edge_three'];
-//                $readyProduct->part_edge_four = $product['part_edge_four'];
-//
-//                $readyProduct->edge_one = $product['edge_one'];
-//                $readyProduct->edge_two = $product['edge_two'];
-//                $readyProduct->edge_three = $product['edge_three'];
-//                $readyProduct->edge_four = $product['edge_four'];
-//
-//                $readyProduct->nip_id = $product['nip_id'];
-//                $readyProduct->width = $product['width'];
-//                $readyProduct->length = $product['length'];
-//                $readyProduct->coast = $product['coast'];
-//                $readyProduct->save();
-//
-//                $coast[] = (int)$product['coast'];
-//
-//                DB::table('order_ready_product')->insert([
-//                    'order_id' => $order->id,
-//                    'ready_product_id' => $readyProduct->id
-//                ]);
-//            }
-//
-//            $order->blank = $request->blank_sum;
-//            $order->order_amount = array_sum($coast)+$request->blank_sum;
-//            $order->save();
-//
-//            return response()->json(['status' => true]);
-//        }
-//
-//        return response()->json(['status' => false]);
+            foreach ($request->ready_product as $product){
+
+                $readyProduct = new ReadyProduct();
+                $readyProduct->blank_type_id = $product['blank_type_id'];
+                $readyProduct->thickness_id = $product['thickness_id'];
+                $readyProduct->form_id = $product['form_id'];
+                $readyProduct->decor_category_id = $product['decor_category_id'];
+                $readyProduct->decor_id = $product['decor_id'];
+                $readyProduct->wrapper_id = $product['wrapper_id'];
+                $readyProduct->pattern_accordance_id = $product['pattern_accordance_id'];
+
+                $readyProduct->part_side_one = $product['part_side_one'];
+                $readyProduct->part_side_two = $product['part_side_two'];
+                $readyProduct->part_side_three = $product['part_side_three'];
+                $readyProduct->part_side_four = $product['part_side_four'];
+                $readyProduct->part_edge_one = $product['part_edge_one'];
+                $readyProduct->part_edge_two = $product['part_edge_two'];
+                $readyProduct->part_edge_three = $product['part_edge_three'];
+                $readyProduct->part_edge_four = $product['part_edge_four'];
+
+                $readyProduct->edge_one = $product['edge_one'];
+                $readyProduct->edge_two = $product['edge_two'];
+                $readyProduct->edge_three = $product['edge_three'];
+                $readyProduct->edge_four = $product['edge_four'];
+
+                $readyProduct->nip_id = $product['nip_id'];
+                $readyProduct->width = $product['width'];
+                $readyProduct->length = $product['length'];
+                $readyProduct->coast = $product['coast'];
+                $readyProduct->save();
+
+                $coast[] = (int)$product['coast'];
+
+                DB::table('order_ready_product')->insert([
+                    'order_id' => $order->id,
+                    'ready_product_id' => $readyProduct->id
+                ]);
+            }
+
+            $order->blank = $request->blank_sum;
+            $order->order_amount = array_sum($coast)+$request->blank_sum;
+            $order->save();
+
+            return response()->json(['status' => true]);
+        }
+
+        return response()->json(['status' => false]);
     }
 }
