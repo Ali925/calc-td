46c4dc88c6fcfe7a6934c51d74fcc9870ec3b869
Andy Melentev <melentev.av@gmail.com>
2017-01-26 19:40:36 +0500

    fix pattern screen

--- resources/views/partials/edge.blade.php
+++ resources/views/partials/edge.blade.php
@@ -1,7 +1,7 @@
 <div class="calc-mobal-blc calc-js-modal-parent" id="calc-js-step2_3_modal">  <!-- модальное окно для выбора кромок -->
     <div class="calc-modal-close">
         <i class="fa fa-times" aria-hidden="true"></i>
-        закрыть
+        ПОДТВЕРДИТЬ
     </div>
     <div class="calc-modal-inside">
         @foreach($edges as $edge)
