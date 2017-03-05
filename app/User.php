57817e9bf7ffa71e50eac296ff9192eef997a9ec
Andy Melentev <melentev.av@gmail.com>
2017-03-05 21:35:49 +0500

    bcrypt user pass

--- app/User.php
+++ app/User.php
@@ -48,4 +48,9 @@ class User extends Authenticatable
     public function isManager(){
         return $this->role === 2;
     }
+
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = bcrypt($value);
+    }
 }
