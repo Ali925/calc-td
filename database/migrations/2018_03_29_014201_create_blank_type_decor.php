a2a2e6acadb57ef92c561efb92b1fb216b786ec3
Ali925 <alitural.mehdi@gmail.com>
2018-03-29 05:43:21 +0400

    add decor to blank

---
+++ database/migrations/2018_03_29_014201_create_blank_type_decor.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBlankTypeDecor extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+         Schema::drop('blank_type_decors');
+
+         Schema::create('blank_type_decor', function (Blueprint $table){
+            $table->unsignedInteger('blank_type_id');
+            $table->unsignedInteger('decor_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blank_type_decors');
+        Schema::dropIfExists('blank_type_decor');
+    }
+}
