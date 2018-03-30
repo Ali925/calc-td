9e676f13ca7136ca44e08942e24da6c1259898a0
Ali925 <alitural.mehdi@gmail.com>
2018-03-31 00:11:44 +0400

    all button

--- public/js/select-all.js
+++ public/js/select-all.js
@@ -1,12 +1,19 @@
 $( document ).ready(function() {
 	$(".checkbox>label").click(function(){
-		console.log('aaa');
-		$(".input-select[name='decors[]'] option").prop('selected', true);
-		$(".input-select[name='decors[]'] option").each(function(){
-			var text = "<li class='select2-selection__choice' title='"+$(this).text()+"'>" +
-						"<span class='select2-selection__choice__remove' role='presentation'>x</span>"+
-						"</li>";
-			$(".input-select[name='decors[]']").next().find("ul").prepend(text);
-		});
+		var self = $(this);
+		if(self.find("input").is(':checked')){
+			console.log('aaa');
+			$(".input-select[name='decors[]']").next().find("li.select2-selection__choice").remove();
+			$(".input-select[name='decors[]'] option").prop('selected', true);
+			$(".input-select[name='decors[]'] option").each(function(){
+				var text = "<li class='select2-selection__choice' title='"+self.text()+"'>" +
+							"<span class='select2-selection__choice__remove' role='presentation'>x</span>"+
+							self.text() + "</li>";
+				$(".input-select[name='decors[]']").next().find("ul").prepend(text);
+			});
+		} else {
+			$(".input-select[name='decors[]'] option").prop('selected', false);
+			$(".input-select[name='decors[]']").next().find("li.select2-selection__choice").remove();
+		}
 	});
 });
