a0f92e36c71d185cf98da53c712e13c5f55a0a24
Ali925 <alitural.mehdi@gmail.com>
2018-03-31 00:13:47 +0400

    all button

--- public/js/select-all.js
+++ public/js/select-all.js
@@ -1,14 +1,14 @@
 $( document ).ready(function() {
 	$(".checkbox>label").click(function(){
 		var self = $(this);
-		if(self.find("input").is(':checked')){
+		if($(this).find("input").is(':checked')){
 			console.log('aaa');
 			$(".input-select[name='decors[]']").next().find("li.select2-selection__choice").remove();
 			$(".input-select[name='decors[]'] option").prop('selected', true);
 			$(".input-select[name='decors[]'] option").each(function(){
-				var text = "<li class='select2-selection__choice' title='"+self.text()+"'>" +
+				var text = "<li class='select2-selection__choice' title='"+$(this).text()+"'>" +
 							"<span class='select2-selection__choice__remove' role='presentation'>x</span>"+
-							self.text() + "</li>";
+							$(this).text() + "</li>";
 				$(".input-select[name='decors[]']").next().find("ul").prepend(text);
 			});
 		} else {
