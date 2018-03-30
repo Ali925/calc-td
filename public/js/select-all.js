1dac9a6f6e3602796c1a5928f283465384654b11
Ali925 <alitural.mehdi@gmail.com>
2018-03-30 23:57:57 +0400

    all button

--- public/js/select-all.js
+++ public/js/select-all.js
@@ -1,8 +1,12 @@
 $( document ).ready(function() {
 	$(".checkbox>label").click(function(){
 		console.log('aaa');
+		$(".input-select[name='decors[]'] option").prop('selected', true);
 		$(".input-select[name='decors[]'] option").each(function(){
-			$(this).click();
+			var text = "<li class='select2-selection__choice' title='"+$(this).text()+"'>" +
+						"<span class='select2-selection__choice__remove' role='presentation'>x</span>"+
+						"</li>";
+			$(".input-select[name='decors[]']").next().find("ul").prepend(text);
 		});
 	});
 });
