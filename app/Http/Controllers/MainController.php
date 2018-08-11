0e750162ca4d76c8da57cb5ea3c8fc372c50f7c4
Ali925 <alitural.mehdi@gmail.com>
2018-08-11 04:27:36 +0400

    notifications

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -30,7 +30,7 @@ class MainController extends Controller
         $response['thickness'] = Thickness::with('nips')->get();
         $response['nip'] = Nip::with('patternPositions')->get();
         $response['wrapper'] = Wrapper::all();
-        $response['error_nots'] = ErrorNotifications::where("type", 2)->get();
+        $response['errors'] = ErrorNotifications::all();
 
         return response()->json($response);
     }
